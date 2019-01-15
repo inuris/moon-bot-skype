917097750fe94b674f26587920589295d1baba04
KHOA-PC\Khoa <lakhoa1989@gmail.com>
2019-01-15 18:38:32 +0700

    merge request to core

--- bot.js
+++ bot.js
@@ -21,33 +21,34 @@ class MyBot {
                 var website= new Website(turnContext.activity.text);
                 // Nếu có trong list website thì mới trả lời
                 if (website.found === true){
-                    const message = await new Promise(resolve => {                        
-                        var requestOptions = {
-                            method: "GET",
-                            url: website.url,
-                            gzip: true
-                        };
-                        // Nếu website cần Cookie thì set
-                        if (website.cookie !== null){
-                            var cookie = request.cookie(website.cookie);
-                            requestOptions.headers = {
-                                'Cookie': cookie
-                            };
-                            requestOptions.jar = true;
-                        }
-                        request(requestOptions, function(error, response, body) {
-                            // Đưa html raw vào website
-                            website.setHtmlRaw(body);  
-                            var item = new Item(website);                 
-                            // Log to file
-                            var logtype='info';
-                            if (item.weight.value===0 || item.category.ID === "UNKNOWN") {logtype='error';}
-                            logger.log(logtype,'{\n"URL":"%s",\n"PRICE":"%s",\n"SHIPPING":"%s",\n"WEIGHT":"%s",\n"CATEGORY":"%s",\n"TOTAL":"%s",\n"CATEGORYSTRING":"%s"\n}', website.url, item.price.string, item.shipping.string,item.weight.current,item.category.att.ID,item.totalString,item.category.string);
+                    // const message = await new Promise(resolve => {                        
+                    //     var requestOptions = {
+                    //         method: "GET",
+                    //         url: website.url,
+                    //         gzip: true
+                    //     };
+                    //     // Nếu website cần Cookie thì set
+                    //     if (website.cookie !== null){
+                    //         var cookie = request.cookie(website.cookie);
+                    //         requestOptions.headers = {
+                    //             'Cookie': cookie
+                    //         };
+                    //         requestOptions.jar = true;
+                    //     }
+                    //     request(requestOptions, function(error, response, body) {
+                    //         // Đưa html raw vào website
+                    //         website.setHtmlRaw(body);  
+                    //         var item = new Item(website);                 
+                    //         // Log to file
+                    //         var logtype='info';
+                    //         if (item.weight.value===0 || item.category.ID === "UNKNOWN") {logtype='error';}
+                    //         logger.log(logtype,'{\n"URL":"%s",\n"PRICE":"%s",\n"SHIPPING":"%s",\n"WEIGHT":"%s",\n"CATEGORY":"%s",\n"TOTAL":"%s",\n"CATEGORYSTRING":"%s"\n}', website.url, item.price.string, item.shipping.string,item.weight.current,item.category.att.ID,item.totalString,item.category.string);
                     
-                            var _response= item.toText();
-                            resolve(_response);             
-                        });  
-                    })
+                    //         var _response= item.toText();
+                    //         resolve(_response);             
+                    //     });  
+                    // })
+                    var message = website.getResponse();
                     return turnContext.sendActivity(message);                   
                 }
                 if (website.isUrl === true)
