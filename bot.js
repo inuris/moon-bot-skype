8f3338625e1c7c53caf20e00b2d6621967f467ac
KHOA-PC\Khoa <lakhoa1989@gmail.com>
2019-01-15 19:36:53 +0700

    fix bug

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ class MyBot {
                 var website= new Website(turnContext.activity.text);
                 // Nếu có trong list website thì mới trả lời
                 if (website.found === true){
-                    var message = website.getResponse();
+                    var message = Wbsite.getResponse(website);
                     return turnContext.sendActivity(message);                   
                 }
                 if (website.isUrl === true)
