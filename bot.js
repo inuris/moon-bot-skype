ec02b1b2c359b4cba8906f4085860a2471b51cb8
KHOA-PC\Khoa <lakhoa1989@gmail.com>
2019-01-19 10:37:34 +0700

    update command !list

--- bot.js
+++ bot.js
@@ -15,21 +15,27 @@ class MyBot {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types. 
         const response = async (turnContext) => {
             if (turnContext.activity.type === ActivityTypes.Message) {
-                var website= new Website(turnContext.activity.text);
-                // Nếu có trong list website thì mới trả lời
-                if (website.found === true){
-                    var item = await Website.getItem(website);
-                    // Nếu ko lấy được giá thì có thể là 3rd Seller (Amazon)
-                    if (item.price.value==0 && item.redirect!=="")
-                    {
-                        console.log("Found redirect");
-                        website= new Website(item.redirect);
-                        item = await Website.getItem(website, item);
-                    }
-                    return turnContext.sendActivity(item.toText());                   
+                if (turnContext.activity.text==="!list"){
+                    return turnContext.sendActivity("Moon hỗ trợ báo giá các web sau: " + Website.getAvailableWebsite()); 
                 }
-                if (website.isUrl === true)
-                    return turnContext.sendActivity("Xin lỗi, Moon chỉ hỗ trợ báo giá các web sau: " + Website.getAvailableWebsite());                          
+                else
+                {
+                    var website= new Website(turnContext.activity.text);
+                    // Nếu có trong list website thì mới trả lời
+                    if (website.found === true){
+                        var item = await Website.getItem(website);
+                        // Nếu ko lấy được giá thì có thể là 3rd Seller (Amazon)
+                        if (item.price.value==0 && item.redirect!=="")
+                        {
+                            console.log("Found redirect");
+                            website= new Website(item.redirect);
+                            item = await Website.getItem(website, item);
+                        }
+                        return turnContext.sendActivity(item.toText());                   
+                    }
+                    if (website.isUrl === true)
+                        return turnContext.sendActivity("Xin lỗi, Moon chỉ hỗ trợ báo giá các web sau: " + Website.getAvailableWebsite()); 
+                }                         
             } 
             else {
                 //await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
