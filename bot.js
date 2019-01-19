8be36bef6df6ae9a4208d0434a2a6a7067367a89
KHOA-PC\Khoa <lakhoa1989@gmail.com>
2019-01-19 09:07:38 +0700

    fix redirect

--- bot.js
+++ bot.js
@@ -18,15 +18,15 @@ class MyBot {
                 var website= new Website(turnContext.activity.text);
                 // Nếu có trong list website thì mới trả lời
                 if (website.found === true){
-                    var message = await Website.getResponse(website);
+                    var item = await Website.getItem(website);
                     // Nếu ko lấy được giá thì có thể là 3rd Seller (Amazon)
-                    if (message.price.value==0 && message.redirect!=="")
+                    if (item.price.value==0 && item.redirect!=="")
                     {
                         console.log("Found redirect");
-                        website= new Website(message.redirect);
-                        message = await Website.getResponse(website);
+                        website= new Website(item.redirect);
+                        item = await Website.getItem(website);
                     }
-                    return turnContext.sendActivity(message.toText());                   
+                    return turnContext.sendActivity(item.toText());                   
                 }
                 if (website.isUrl === true)
                     return turnContext.sendActivity("Xin lỗi, Moon chỉ hỗ trợ báo giá các web sau: " + Website.getAvailableWebsite());                          
