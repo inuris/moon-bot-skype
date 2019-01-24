141ed0d5be004df369b4498a560cfcb95602b42c
Khoa Lam <lakhoa1989@gmail.com>
2019-01-24 14:43:15 +0700

    update logger to discord

--- bot.js
+++ bot.js
@@ -3,7 +3,15 @@
 
 const { ActivityTypes } = require('botbuilder');
 
+const DiscordLogger = require('discord-logger');
+const options = {
+  endpoint: process.env.DISCORD_WEBHOOK,
+  botUsername: 'Logger'
+} 
+const logger = new DiscordLogger(options);
+
 const Website = require("./core/moon.js").Website;
+
 // Imports dependencies and set up http server
 
 class MyBot {
@@ -24,12 +32,18 @@ class MyBot {
                     // Nếu có trong list website thì mới trả lời
                     if (website.found === true){
                         var item = await Website.getItem(website);
+                        var log=item.toLog();
+                        if (log.type==="error") logger.error(log.content);
+                        else logger.success(log.content);
                         // Nếu ko lấy được giá thì có thể là 3rd Seller (Amazon)
                         if (item.price.value==0 && item.redirect!=="")
                         {
                             console.log("Found redirect");
                             website= new Website(item.redirect);
                             item = await Website.getItem(website, item);
+                            log=item.toLog();
+                            if (log.type==="error") logger.error(log.content);
+                            else logger.success(log.content);
                         }
                         return turnContext.sendActivity(item.toText());                   
                     }
